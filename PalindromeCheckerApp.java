54d3da456e9ba6fa09b6e98131abc78732ba12c9
Akash Datta <76147605+ShinyACash@users.noreply.github.com>
2026-02-17 14:42:41 +0530

    shiny  Added UC1

---
+++ PalindromeCheckerApp.java
@@ -0,0 +1,36 @@
+/*
+* ==========================================
+*   MAIN CLASS - PalindromeCheckerApp.java
+* ==========================================
+*
+* Use Case 1: Application Entry & Welcome Message
+*
+* At this stage, the application:
+*  - Starts execution from the main() method
+*  - Displays a welcome message
+*  - Shows application version
+*
+* No palindrome logic is implemented yet.
+*
+* The goal is to establish a clear startup flow.
+*
+* @author shiny
+* @version 1.0.0
+*/
+
+public class PalindromeCheckerApp{
+
+    /*
+    * Application entry point
+    *
+    * This is the first method executed by the JVM
+    * when the program starts
+    *
+    * @params args Command-line arguments
+    *
+    */
+
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrome Checker App.\nVersion : 1.0.0\nSystem Initialized successfully");
+    }
+}
